8a1ca7c00f7f20279ee544698713301ab54dc550
Sean Lander <slander36@gmail.com>
2011-10-06 01:55:00 -0500

    changes to make the code more portable

--- src/run.js
+++ src/run.js
@@ -1,14 +1,22 @@
 window.onload = function() {
-	var toolbarElement = document.getElementById("toolbar");
+	var toolbarElement = document.createElement('div');
+	toolbarElement.id = 'toolbar';
+	
+	//var toolbarElement = document.getElementById("toolbar");
 	
 	var button1 = new fsg.h1();
-	button1.text("Header1");
+	button1.text("Headers");
 	toolbarElement.appendChild(button1.element);
 	
 	var button2 = new fsg.ul();
-	button2.text("List *");
+	button2.text("Lists");
 	toolbarElement.appendChild(button2.element);
 	
-	var editElement = document.getElementById("edit");
+	//var editElement = document.getElementById("edit");
+	var editElement = document.createElement('div');
+	editElement.id = 'edit';
 	var edit = new fsg.edit(editElement);
+	
+	document.getElementById('content').appendChild(toolbarElement);
+	document.getElementById('content').appendChild(editElement);
 };
